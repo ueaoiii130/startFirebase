59e38ee9a84cc852fe575500e5e3b0105a3e5ce6
阪上葵 <sakaueaoi@M-Aoi-Sakaue-no-MacBook-puro.local>
2018-08-25 23:16:27 +0900

    initial commit

--- startFirebase/AppDelegate.swift
+++ startFirebase/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+// Firebase導入
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Firebase導入
+        FirebaseApp.configure()
         // Override point for customization after application launch.
         return true
     }
--- startFirebase/ViewController.swift
+++ startFirebase/ViewController.swift
@@ -7,14 +7,49 @@
 //
 
 import UIKit
+// Firebase導入
+import Firebase
 
 class ViewController: UIViewController {
 
+    @IBOutlet var textView: UITextView!
+    @IBOutlet var nameTextField: UITextField!
+    @IBOutlet var messageTextField:UITextField!
+    
+    var databaseRef: FIRDatabaseReference!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+    
+        //インスタンスを作成
+        databaseRef = FIRDatabase.database().reference()
+        
+        databaseRef.observeEventType(.ChildAdded, withBlock: { snapshot in
+            if let name = snapshot.value!.objectForKey("name") as? String,
+                let message = snapshot.value!.objectForKey("message") as? String {
+                self.textView.text = "\(self.textView.text)\n\(name) : \(message)"
+            }
+        })
+    }
+    
+    func textFieldShouldReturn(_ textField:UITextField) -> Bool { //Returnキー
+       
+    }
+    func textFieldShouldReturn(textField: UITextField) -> Bool{
+        
+        let messageData = ["name": nameTextField.text!, "message": messageTextField.text!]
+        databaseRef.childByAutoId().setValue(messageData)
+        
+        textField.resignFirstResponder()
+        messageTextField.text = ""
+        
+        return true
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
